23d8f0a19e9fdb889104a1ea9e1597b6ad364aae
Pratham Mehta <prathamtheempowerer@Prathams-MacBook-Pro-2.local>
2014-01-15 22:10:27 +0530

    Initial Commit

---
+++ Riviera/RivieraViewController.m
@@ -0,0 +1,29 @@
+//
+//  RivieraViewController.m
+//  Riviera
+//
+//  Created by Pratham Mehta on 15/01/14.
+//  Copyright (c) 2014 Pratham Mehta. All rights reserved.
+//
+
+#import "RivieraViewController.h"
+
+@interface RivieraViewController ()
+
+@end
+
+@implementation RivieraViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
